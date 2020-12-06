abed1ffd1696e27675b7a840136ef946726a3804
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2020-12-06 21:08:09 +0100

    added credentials file

---
+++ public/lib/credentials.js
@@ -0,0 +1,21 @@
+module.exports = {
+    cookiesecret:'pawklan_cookieSecret12345',
+    gmail:{
+        user:'okeibunoremma@gmail.com',
+        password:'ilove2000'
+    },
+    mongo: {
+        development: {
+            connectionString:"mongodb://127.0.0.1:27017/pawklan"
+        },
+        production: {
+            connectionString:"mongodb://127.0.0.1:27017/pawklan"
+        },
+    },
+    oauth: {
+        clientID: "692249458486-643j5hcss17nih3102b03cfrgami2lre.apps.googleusercontent.com",
+        clientSecret:"ZsdC55S9B2n2DLArqxNxd3ys"
+    },
+    secretCrypt: 'pawklan_secretKey',
+    oxr_auth: "7465092a8bcb445e95d2b647396a8be0"
+}
